1cae7f6a859669ba6799859b22ac13ebd0a19e9a
AppaniSandeep <appanisandeep18@gmail.com>
2026-04-24 17:33:11 +0530

    Add deleteUser function to AuthController

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -57,9 +57,22 @@ const home = async (req, res) => {
     }
 }
 
+const deleteUser = async (req, res) => {
+    try{
+        const {id} = req.params;
+        const user = await UserModel.findByIdAndDelete(id);
+        if (!user){
+            return res.status(404).json({message:"User not found", success:false})
+        }
+        res.status(200).json({message:"User deleted successfully", success:true})
+    }catch(e){
+        res.status(500).json({message:"Internal Server Error", success:false})
+    }
+}
 
 module.exports = {
     signup,
     login,
-    home
+    home,
+    deleteUser
 }
