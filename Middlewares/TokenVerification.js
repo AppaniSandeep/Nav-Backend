1b54c7dd41a10bf727079d7dc0292b32980d2fdf
AppaniSandeep <appanisandeep18@gmail.com>
2026-04-27 12:33:40 +0530

    Correct typo in token extraction
    Fix typo in variable name from 'athHeader' to 'authHeader'.

--- Middlewares/TokenVerification.js
+++ Middlewares/TokenVerification.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
-  const token = authHeader && athHeader.split(" ")[1];
+  const token = authHeader && authHeader.split(" ")[1];
 
   if(!token){
     return res.status(403).json({message:"No token provided authorization denied", success:false})
