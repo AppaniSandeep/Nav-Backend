6bf6e64623597ec0bd6d4c231983d195c92d3ff5
AppaniSandeep <appanisandeep18@gmail.com>
2026-04-27 12:26:08 +0530

    Add token verification to home route

--- Routes/AuthRouter.js
+++ Routes/AuthRouter.js
@@ -1,5 +1,6 @@
 const { signup, login, home, deleteUser } = require("../Controllers/AuthController");
 const { loginValidation, signupValidation } = require("../Middlewares/AuthValidation");
+const {verifyToken} = require("../Middlewares/TokenVerification");
 
 const router = require("express").Router();
 
@@ -7,7 +8,7 @@ router.post("/login",loginValidation,login)
 
 router.post("/signup",signupValidation, signup);
 
-router.get("/home", home);
+router.get("/home",verifyToken, home);
 
 router.delete("/delete/:id", deleteUser);
 
